c618a4c500861cf8b6a36300867911a5fd5955cd
Cristiano Aguiar <cristiano@b206.com>
2015-05-19 17:03:32 -0300

    Plot5-Plot6

--- plot5.R
+++ plot5.R
@@ -8,10 +8,13 @@ NEI <- tbl_df(NEI)
 SCC <- tbl_df(SCC)
 
 ## Filter SCC for coal related emissions 
-SCC <- filter(SCC,grepl("coal",Short.Name,ignore.case=TRUE))
+SCC <- filter(SCC,grepl("vehicle",EI.Sector,ignore.case=TRUE))
 
 ## Subsetting NEI by coal related SCC
 NEI <- NEI[NEI$SCC %in% SCC$SCC,]
+
+## Filter for Baltimore City 
+NEI <- filter(NEI,fips=="24510")
 
 ## Grouping table by year
 NEI <- group_by(NEI,year)
@@ -20,7 +23,7 @@ NEI <- group_by(NEI,year)
 NEI_sum <- summarize(NEI,total = sum(Emissions))
 
 ## Redirecting plot output to png
-png(filename = "plot4.png",width = 480, height = 480, units = "px",bg = "white")
+png(filename = "plot5.png",width = 480, height = 480, units = "px",bg = "white")
 
 g <- ggplot(NEI_sum, aes(year, total))
 g <- g + 
@@ -28,7 +31,7 @@ g <- g +
   geom_smooth(method="lm") + # Linear model
   labs(x = "Year") + # X label
   labs(y = "Total of emissions (ton)") + # Y label
-  labs(title = "Coal combustion-related sources in United States") # Title
+  labs(title = "Emissions from motor vehicles sources in Baltimore City") # Title
 
 print(g)
 dev.off()
--- plot6.R
+++ plot6.R
@@ -8,27 +8,36 @@ NEI <- tbl_df(NEI)
 SCC <- tbl_df(SCC)
 
 ## Filter SCC for coal related emissions 
-SCC <- filter(SCC,grepl("coal",Short.Name,ignore.case=TRUE))
+SCC <- filter(SCC,grepl("vehicle",EI.Sector,ignore.case=TRUE))
 
 ## Subsetting NEI by coal related SCC
 NEI <- NEI[NEI$SCC %in% SCC$SCC,]
 
+## Filter for Baltimore City or Los Angeles County
+NEI <- filter(NEI,fips=="24510" | fips=="06037")
+
 ## Grouping table by year
-NEI <- group_by(NEI,year)
+NEI <- group_by(NEI,year,fips)
 
 ## Summarizing data using group above and getting total of each group (year)
 NEI_sum <- summarize(NEI,total = sum(Emissions))
 
+## Changing code to name
+NEI_sum[which(NEI_sum$fips=="24510"),"fips"] <- "Baltimore"
+NEI_sum[which(NEI_sum$fips=="06037"),"fips"] <- "Los Angeles"
+
 ## Redirecting plot output to png
-png(filename = "plot4.png",width = 480, height = 480, units = "px",bg = "white")
+png(filename = "plot6.png",width = 480, height = 480, units = "px",bg = "white")
 
 g <- ggplot(NEI_sum, aes(year, total))
 g <- g + 
-  geom_point() + # Adding points
-  geom_smooth(method="lm") + # Linear model
+  geom_point(aes(color=fips)) + # Adding points
+  geom_line(aes(color=fips)) +
   labs(x = "Year") + # X label
   labs(y = "Total of emissions (ton)") + # Y label
-  labs(title = "Coal combustion-related sources in United States") # Title
+  labs(title = "Emissions from motor vehicles sources") + # Title
+  theme(legend.title=element_blank()) 
+  
 
 print(g)
 dev.off()
